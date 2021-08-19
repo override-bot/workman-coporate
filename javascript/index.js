c3eb42fec5cdeef133277dca0468e9e456716e4a
Chukwudi Wisdom <override-bot@users.noreply.github.com>
2021-08-19 03:44:30 +0200

    update

--- javascript/index.js
+++ javascript/index.js
@@ -9,3 +9,18 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+//send message
+function sendMessage() {
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    var name = document.getElementById('name').value;
+    if (email.length > 6 && message.length > 6) {
+        db.collection('userMessages').add({
+            email: email,
+            name: name,
+            message: message
+        });
+        alert("message sent succesfully")
+    }
+}
